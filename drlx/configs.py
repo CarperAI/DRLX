334d7a347d5c826ac1720318ac5340ab753fa1da
shahbuland <smatiana1@gmail.com>
2023-06-30 18:47:34 -0400

    Add config fields for wandb and grad clip

--- drlx/configs.py
+++ drlx/configs.py
@@ -19,6 +19,7 @@ class ModelConfig(ConfigClass):
 @dataclass
 class TrainConfig(ConfigClass):
     batch_size : int = 64
+    epochs : int = 2
     
     #Optimizer
     learning_rate : float = 1e-4
@@ -32,13 +33,22 @@ class TrainConfig(ConfigClass):
     log_every : int = 10
     save_every : int = 10
     save_to : str = "checkpoints"
+    load_from : str = None # If this is not none, a checkpoint is loaded
     eval_every : int = 10
     sample_every : int = 10
+    
+    grad_clip : float = 1.0
+
+    # WANDB
+    run_name : str = None
+    wandb_entity : str = None
+    wandb_project : str = None
 
 @dataclass
 class SamplerConfig(ConfigClass):
     mode : str = "v" # x, v, or eps
     guidance_scale : float = None # if guidance is being used
+    sigma_data : float = 0.5 # Estimated sd for data
 
 
 def load_yaml(yml_fp : str) -> Dict[str, ConfigClass]:
