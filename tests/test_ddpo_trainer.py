5b263d117efcc135ab43206f7aa44ca39b777d22
shahbuland <smatiana1@gmail.com>
2023-07-25 20:10:22 +0000

    Add example config and training example

---
+++ tests/test_ddpo_trainer.py
@@ -0,0 +1,15 @@
+from drlx.trainer.ddpo_trainer import DDPOTrainer
+from drlx.configs import DRLXConfig
+from drlx.reward_modelling.toy_rewards import AverageBlueReward
+
+# Pipeline first
+from drlx.pipeline.pickapic_prompts import PickAPicPrompts
+
+import torch
+
+pipe = PickAPicPrompts()
+
+config = DRLXConfig.load_yaml("configs/ddpo_sd.yml")
+trainer = DDPOTrainer(config)
+
+trainer.train(pipe, AverageBlueReward())
