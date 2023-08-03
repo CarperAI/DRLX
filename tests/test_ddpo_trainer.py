63db8d637485e843f31093d1cb26e0d76abcca97
shahbuland <smatiana1@gmail.com>
2023-08-03 19:58:51 +0000

    Add resuming runs as option in example script

--- tests/test_ddpo_trainer.py
+++ tests/test_ddpo_trainer.py
@@ -1,6 +1,8 @@
 from drlx.trainer.ddpo_trainer import DDPOTrainer
 from drlx.configs import DRLXConfig
 from drlx.reward_modelling.toy_rewards import JPEGCompressability
+from drlx.reward_modelling.aesthetics import Aesthetics
+from drlx.utils import get_latest_checkpoint
 
 # Pipeline first
 from drlx.pipeline.pickapic_prompts import PickAPicPrompts
@@ -8,8 +10,13 @@ from drlx.pipeline.pickapic_prompts import PickAPicPrompts
 import torch
 
 pipe = PickAPicPrompts()
+resume = False
 
 config = DRLXConfig.load_yaml("configs/ddpo_sd.yml")
 trainer = DDPOTrainer(config)
 
-trainer.train(pipe, JPEGCompressability())
+if resume:
+    cp_dir = get_latest_checkpoint(f"checkpoints/{config.logging.run_name}")
+    trainer.load_checkpoint(cp_dir)
+
+trainer.train(pipe, Aesthetics())
